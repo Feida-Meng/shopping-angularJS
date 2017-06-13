4cbe929d88be538b631c5e021357ac7ba2216031
codonculous <feidameng01@gmail.com>
2017-06-13 16:32:46 -0400

    created and set up items component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from "@angular/http";
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ItemsComponent } from './items/items.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
